5409723e224e605d8822636c47952780abf9e48b
toomuchtearz <toomuchtearz@gmail.com>
2026-03-23 13:35:57 +0200

    register urls for events

---
+++ events/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from events.views import EventViewSet
+
+app_name = "events"
+
+router = routers.DefaultRouter()
+router.register("events", EventViewSet, basename="events")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
