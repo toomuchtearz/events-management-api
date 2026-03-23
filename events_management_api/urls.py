a0db4c0a6e51fcf23e88be8ee9aab141a9908aaa
toomuchtearz <toomuchtearz@gmail.com>
2026-03-23 18:24:48 +0200

    change urls structure

--- events_management_api/urls.py
+++ events_management_api/urls.py
@@ -8,19 +8,24 @@ from drf_spectacular.views import (
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
-urlpatterns = [
+api_v1_patterns = [
     path('admin/', admin.site.urls),
 
-    path("api/v1/events/", include("events.urls",namespace="events")),
-    path("api/v1/users/", include("users.urls",namespace="users")),
+    path("events/", include("events.urls", namespace="events")),
+    path("users/", include("users.urls", namespace="users")),
 
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # Auth
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    # YOUR PATTERNS
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # Docs
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(api_v1_patterns))
 ]
