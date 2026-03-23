f91048efc4ccfe068049b7257906f8017222c1f7
toomuchtearz <toomuchtearz@gmail.com>
2026-03-23 13:28:08 +0200

    create serializer & view for Event

--- events/views.py
+++ events/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from events.models import Event
+
+from events.serializers import EventSerializer
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Event.objects.select_related(
+            "organizer"
+        ).prefetch_related("attendees")
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(organizer=self.request.user)
