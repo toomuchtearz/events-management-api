b6ca914f9d3b91af9fbabfe259c7bd93f5588cb5
toomuchtearz <toomuchtearz@gmail.com>
2026-03-23 14:43:54 +0200

    add toggle-register, my & attending custom actions

--- events/views.py
+++ events/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets
+from django.db.models import Count
+from rest_framework import viewsets, status, serializers
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from events.models import Event
 from events.permissions import IsOrganizerOrReadOnly
@@ -16,9 +19,9 @@ class EventViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsOrganizerOrReadOnly)
 
     def get_queryset(self):
-        queryset = Event.objects.select_related(
-            "organizer"
-        ).prefetch_related("attendees")
+        queryset = Event.objects.select_related("organizer").annotate(
+            attendees_count=Count("attendees")
+        )
 
         return queryset
 
@@ -33,3 +36,37 @@ class EventViewSet(viewsets.ModelViewSet):
             serializer = EventRetrieveSerializer
 
         return serializer
+
+    @action(
+        detail=True,
+        methods=["POST"],
+        permission_classes=(IsAuthenticated,),
+        serializer_class=serializers.Serializer,
+        url_path="toggle-register"
+    )
+    def toggle_register(self, request, pk=None):
+        event = self.get_object()
+        user = request.user
+
+        if user in event.attendees.all():
+            event.attendees.remove(user)
+            return Response({"detail": "Successfully unregistered from the event."}, status=status.HTTP_200_OK)
+        else:
+            event.attendees.add(user)
+            return Response({"detail": "Successfully registered for the event."}, status=status.HTTP_200_OK)
+
+
+    @action(detail=False, methods=["GET"], permission_classes=(IsAuthenticated,))
+    def my(self, request):
+        my_events = self.get_queryset().filter(organizer=request.user)
+        serializer = self.get_serializer(my_events, many=True)
+
+        return Response(serializer.data)
+
+
+    @action(detail=False, methods=["GET"], permission_classes=(IsAuthenticated,))
+    def attending(self, request):
+        attending_events = self.get_queryset().filter(attendees=request.user)
+        serializer = self.get_serializer(attending_events, many=True)
+
+        return Response(serializer.data)
