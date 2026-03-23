cf2ca9d278406495252dcd5a86483a73f9c42271
toomuchtearz <toomuchtearz@gmail.com>
2026-03-23 13:22:19 +0200

    create Event & User models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    time = models.DateTimeField()
+    location = models.CharField(max_length=64)
+
+    organizer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="events",
+        on_delete=models.CASCADE
+    )
+    attendees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="attended_events",
+        blank=True
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ["-time"]
